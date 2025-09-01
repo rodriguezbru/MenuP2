a0020fb3cb53fe36d79fcf7ed8cb64bba0e8d240
Agustín Montaña Larralde <agustinmont2001@gmail.com>
2025-09-01 13:04:59 -0300

    Implementación clase Waiter

--- src/Waiter.cs
+++ src/Waiter.cs
@@ -7,6 +7,22 @@ namespace Ucu.Poo.Restaurant
     /// </summary>
     public class Waiter
     {
+        private string name;
         private List<Table> assignedTables = new List<Table>();
+        
+        public Waiter(string name)
+        {
+            this.name = name;
+        }
+
+        public void AssignTable(Table table)
+        {
+            assignedTables.Add(table);
+        }
+
+        public void TakeOrder(Table table, Dish dish)
+        {
+            table.Order.Add(dish);
+        }
     }
 }
